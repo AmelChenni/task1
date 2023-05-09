417e98ac200ba5cf41bad6aa6433b28b0a5bb93a
AmelChenni <chenniamel45@gmail.com>
2023-05-08 19:28:28 -0500

    upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import About from './components/About/About.jsx';
+import Home from './components/Home';
+
+function App() {
+  return (
+   <> <About />
+   <Home />
+   </>
+  );
+}
+
+export default App;
